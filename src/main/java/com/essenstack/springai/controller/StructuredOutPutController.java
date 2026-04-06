a84ab4c8a2126d89e1cf27cdb39d42ace93a3279
sandeepn <sandeep@Sandeeps-MacBook-Air.local>
2026-04-06 18:39:34 -0500

    add structured output to llm response

---
+++ src/main/java/com/essenstack/springai/controller/StructuredOutPutController.java
@@ -0,0 +1,46 @@
+package com.essenstack.springai.controller;
+
+
+import com.essenstack.springai.model.CountryCities;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StructuredOutPutController {
+
+    private final ChatClient chatClient;
+
+    public StructuredOutPutController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.defaultAdvisors(new SimpleLoggerAdvisor())
+                .build();
+    }
+
+    @GetMapping("/chat-bean")
+    public ResponseEntity<CountryCities> chatBean(@RequestParam("message") String message) {
+        CountryCities countryCities = chatClient
+                .prompt()
+                .user(message)
+                .call().entity(CountryCities.class);
+        return ResponseEntity.ok(countryCities);
+    }
+
+    @GetMapping("/chat-bean-list")
+    public ResponseEntity<List<CountryCities>> chatBeanList(@RequestParam("message") String message) {
+        List<CountryCities> countryCities = chatClient
+                .prompt()
+                .user(message)
+                .call().entity(new ParameterizedTypeReference<List<CountryCities>>() {
+                });
+        return ResponseEntity.ok(countryCities);
+    }
+
+}
---
+++ src/main/java/com/essenstack/springai/model/CountryCities.java
@@ -0,0 +1,6 @@
+package com.essenstack.springai.model;
+
+import java.util.List;
+
+public record CountryCities(String country, List<String> cities) {
+}
